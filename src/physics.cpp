c7821d89d95104c85656a5b77cb5517a7ccae2ff
Manpos <miquelpostigo@gmail.com>
2017-04-05 00:24:23 +0200

    Non-exploding cloth

--- src/physics.cpp
+++ src/physics.cpp
@@ -20,10 +20,11 @@ vec3 *clothVertexForce;
 
 #pragma region Variables
 
-float separationX = 1, separationY = 1;
+float separationX = 0.5, separationY = 0.5;
 float gravity = -9.8;
 vec3 spherePosition (0.0f, 1.0f, 0.0f);
 float sphereRadius = 1.0f;
+vec3 fixedInitialPosition[2];
 
 #pragma endregion
 
@@ -75,9 +76,11 @@ void IsCollidingBox(Plane plane, vec3 &prevPosition, vec3 &position) {
 	}
 }
 
-void IsCollidingSphere(float radius, vec3 center, vec3 &position) {
+void IsCollidingSphere(float radius, vec3 center, vec3 &position, vec3 prevPos) {
 	vec3 pointSphere(position.x - center.x, position.y - center.y, position.z - center.z);
 	if (sqrt((pointSphere.x * pointSphere.x) + (pointSphere.y * pointSphere.y) + (pointSphere.z * pointSphere.z)) <= radius) {
+		std::cout << position.x << " " << position.y << " " << position.z << std::endl;
+		std::cout << prevPos.x << " " << prevPos.y << " " << prevPos.z << std::endl;
 
 	}
 }
@@ -104,89 +107,64 @@ vec3 * CreateClothMeshArray(int rowVerts, int columnVerts, float vertexSeparatio
 //Particle position vector array, particle velocity vector array, position in array (part1 & part2), original separation (L), damping (d), elasticity (e)
 vec3 forceBetweenParticles(vec3* position, vec3* velocity, int part1, int part2, float L, float d, float e) {
 
-	return -(e * (length(position[part1] - position[part2]) - L) + dot(d * (velocity[part1] - velocity[part2]), ((position[part1] - position[part2]) / length(position[part1] - position[part2])))) * 
+	return -(e * (length(position[part1] - position[part2]) - L) + d * dot((velocity[part1] - velocity[part2]), ((position[part1] - position[part2]) / length(position[part1] - position[part2])))) * 
 		((position[part1] - position[part2]) / length(position[part1] - position[part2]));
 }
 
 
 void springsForceCalc(vec3* position, vec3* velocity, vec3* resultForce, int i, int rows, int cols, float L, float d, float e) {
-	vec3 totalForce = clothVertexForce[i];
-	//Strech 
-	//Horizontal
-	//std::cout << i << std::endl;
-	if (i - 1 * cols >= 0) {
-		//std::cout << "NO LEFT LIMIT" << std::endl;
-		totalForce += forceBetweenParticles(position, velocity, i, i - 1 * cols, L, d, e);
-	}
-	if (i + 1 * cols <= rows * cols - 1) {
-		//std::cout << "NO RIGHT LIMIT" << std::endl;
-		totalForce += forceBetweenParticles(position, velocity, i, i + 1 * cols, L, d, e);
-		//std::cout << totalForce.x << std::endl;
-	}
+	
+	vec3 totalForce = vec3(0.f);
 
-	//Vertical
-	if ((i % cols) - 1 >= 0) {
-		//std::cout << "NO UP LIMIT" << std::endl;
-		totalForce += forceBetweenParticles(position, velocity, i, (i % cols) - 1, L, d, e);
-	}
-	if ((i % cols) + 1 <= cols - 1) {
-		//std::cout << "NO DOWN LIMIT" << std::endl;
-		totalForce += forceBetweenParticles(position, velocity, i, (i % cols) + 1, L, d, e);
-	}
+	//Strech 
+
+	//LEFT
+	if ((i / cols) - 1 >= 0) { totalForce += forceBetweenParticles(position, velocity, i, i - 1 * cols, L, d, e); }
+
+	//RIGHT
+	if ((i / cols) + 1 <= rows - 1) { totalForce += forceBetweenParticles(position, velocity, i, i + 1 * cols, L, d, e); }
+
+	//UP
+	if ((i % cols) - 1 >= 0) { totalForce += forceBetweenParticles(position, velocity, i, i - 1, L, d, e); }
+
+	//DOWN
+	if ((i % cols) + 1 <= cols - 1) { totalForce += forceBetweenParticles(position, velocity, i, i + 1, L, d, e); }
 
 	//Diagonal
-	//Up-left
-	if (i - 1 * cols >= 0 && (i % cols) - 1 >= 0) {
-		//std::cout << "UP-LEFT PARTICLE AVAILABLE" << std::endl;
-		totalForce += forceBetweenParticles(position, velocity, i, i - 1 * cols - 1, sqrt(L*L + L * L), d, e);
-	}
-	
-	//Up-right
-	if (i + 1 * cols <= rows * cols - 1 && (i % cols) - 1 >= 0) {
-		//std::cout << "UP-RIGHT PARTICLE AVAILABLE" << std::endl;
-		totalForce += forceBetweenParticles(position, velocity, i, i + 1 * cols - 1, sqrt(L*L + L * L), d, e);
-	}
 
-	//Down-left
-	if (i - 1 * cols >= 0 && (i % cols) + 1 <= cols - 1) {
-		//std::cout << "DOWN-LEFT PARTICLE AVAILABLE" << std::endl;
-		totalForce += forceBetweenParticles(position, velocity, i, i - 1 * cols + 1, sqrt(L*L + L * L), d, e);
-	}
-	//Down-right
-	if (i + 1 * cols <= rows * cols - 1 && (i % cols) + 1 <= cols - 1) {
-		//std::cout << "DOWN-RIGHT PARTICLE AVAILABLE" << std::endl;
-		totalForce += forceBetweenParticles(position, velocity, i, i + 1 * cols + 1, sqrt(L*L + L * L), d, e);
-	}
+	//UP-LEFT
+	if ((i / cols) - 1 >= 0 && (i % cols) - 1 >= 0) { totalForce += forceBetweenParticles(position, velocity, i, i - 1 * cols - 1, sqrt(L * L + L * L), d, e); }
+	
+	//UP-RIGHT
+	if ((i / cols) + 1 <= rows - 1 && (i % cols) - 1 >= 0) { totalForce += forceBetweenParticles(position, velocity, i, i + 1 * cols - 1, sqrt(L * L + L * L), d, e); }
+
+	//DOWN-LEFT
+	if ((i / cols) - 1 >= 0 && (i % cols) + 1 <= cols - 1) { totalForce += forceBetweenParticles(position, velocity, i, i - 1 * cols + 1, sqrt(L * L + L * L), d, e); }
+
+	//DOWN-RIGHT
+	if ((i / cols) + 1 <= rows - 1 && (i % cols) + 1 <= cols - 1) {	totalForce += forceBetweenParticles(position, velocity, i, i + 1 * cols + 1, sqrt(L * L + L * L), d, e); }
 	
 	//Bending springs
-	//Horizontal
-	//std::cout << i << std::endl;
-	if (i - 2 * cols >= 0) {
-		//std::cout << "NO LEFT LIMIT" << std::endl;
-		totalForce += forceBetweenParticles(position, velocity, i, i - 2 * cols, L * 2, d, e);
 
-		//std::cout << totalForce.x << std::endl;
-	}
-	if (i + 2 * cols <= rows * cols - 1) {
-		//std::cout << "NO RIGHT LIMIT" << std::endl;
-		totalForce += forceBetweenParticles(position, velocity, i, i + 2 * cols, L * 2, d, e);
-	}
+	//LEFT
+	if ((i / cols) - 2 >= 0) { totalForce += forceBetweenParticles(position, velocity, i, i - 2 * cols, L * 2, d, e); }
 
-	//Vertical
-	if ((i % cols) - 2 >= 0) {
-		//std::cout << "NO UP LIMIT" << std::endl;
-		totalForce += forceBetweenParticles(position, velocity, i, (i % cols) - 2, L * 2, d, e);
-	}
-	if ((i % cols) + 2 <= cols - 1) {
-		//std::cout << "NO DOWN LIMIT" << std::endl;
-		totalForce += forceBetweenParticles(position, velocity, i, (i % cols) + 2, L * 2, d, e);
-	}
-	//std::cout << totalForce.x << std::endl;
+	//RIGHT
+	if ((i / cols) + 2 <= rows - 1) { totalForce += forceBetweenParticles(position, velocity, i, i + 2 * cols, L * 2, d, e); }
+
+	//UP
+	if ((i % cols) - 2 >= 0) { totalForce += forceBetweenParticles(position, velocity, i, i - 2, L * 2, d, e); }
+
+	//DOWN
+	if ((i % cols) + 2 <= cols - 1) { totalForce += forceBetweenParticles(position, velocity, i, i + 2, L * 2, d, e); }
+
 	totalForce += vec3(0, gravity, 0);
+
 	resultForce[i] = totalForce;
+	
 }
 
-void verletSolver(vec3 &position, vec3 &prevPosition, vec3 &velocity, vec3 force, float m, float time) {
+void verletSolver(vec3 &position, vec3 &prevPosition, vec3 &velocity, vec3 &force, float m, float time) {
 	//Verlet prev position save
 	vec3 temp = position;
 
@@ -201,15 +179,12 @@ void verletSolver(vec3 &position, vec3 &prevPosition, vec3 &velocity, vec3 force
 
 void PhysicsInit() {
 	vec3 meshPosition ( -4 , 8, -4 );
-	clothVertexPosition = CreateClothMeshArray(ClothMesh::numRows, ClothMesh::numCols, 0.5, 0.5, meshPosition);
+	clothVertexPosition = CreateClothMeshArray(ClothMesh::numRows, ClothMesh::numCols, separationX, separationY, meshPosition);
 	clothVertexPrevPosition = new vec3[ClothMesh::numVerts];
 	clothVertexVelocity = new vec3[ClothMesh::numVerts];
 	clothVertexForce = new vec3[ClothMesh::numVerts];
 
-	clothVertexVelocity[0] = vec3(0, 0, 0);
-	clothVertexVelocity[13] = vec3(0, 0, 0);
 	for (int i = 0; i < ClothMesh::numVerts; ++i) {
-
 		clothVertexPrevPosition[i] = clothVertexPosition[i];
 		clothVertexVelocity[i] = vec3(0, 0, 0);
 		clothVertexForce[i] = vec3(0, 0, 0);
@@ -227,15 +202,21 @@ void PhysicsInit() {
 void PhysicsUpdate(float dt) {
 	//TODO
 	for (int i = 0; i < ClothMesh::numVerts; ++i) {
-
-		if (i != 0 && i != 13) {
-			
-			verletSolver(clothVertexPosition[i], clothVertexPrevPosition[i], clothVertexVelocity[i], clothVertexForce[i], 1, dt);
-			springsForceCalc(clothVertexPosition, clothVertexVelocity, clothVertexForce, i, ClothMesh::numRows, ClothMesh::numCols, separationX, 0.7, 0.7);
-			IsCollidingBox(planeDown, clothVertexPrevPosition[i], clothVertexPosition[i]);
-		}
 		
+		if (i != 0 && i != 13) {
+
+			verletSolver(clothVertexPosition[i], clothVertexPrevPosition[i], clothVertexVelocity[i], clothVertexForce[i], 1, dt);
+			IsCollidingBox(planeDown, clothVertexPrevPosition[i], clothVertexPosition[i]);
+			springsForceCalc(clothVertexPosition, clothVertexVelocity, clothVertexForce, i, ClothMesh::numRows, ClothMesh::numCols, separationX, 2.5, 1);
+
+		}
+
+
 	}
+
+	clothVertexPosition[14];
+	clothVertexVelocity[14];
+	clothVertexForce[14];
 
 	ClothMesh::updateClothMesh(&clothVertexPosition[0].x);
 }
